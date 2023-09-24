870fbc2fbbbfc92d9aec65114e06bf4971e1c1ca
SIXMPRA <145308166+SXMPRA-C@users.noreply.github.com>
2023-09-24 12:07:24 -0600

    Update NeonHub | Bedwars.lua

--- NeonHub | Bedwars.lua
+++ NeonHub | Bedwars.lua
@@ -1,105 +1,27 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Yopapa699/KavoUI/main/KavoUiLib"))()
-local Window = Library.CreateLib("Neon Hub | Bedwars", "DarkTheme")
+-- ths project is lead by Luna_salt
 
-local Main = Window:NewTab("Player")
-local MainSection = Tab:NewSection("Player Scripts")
+-- Library
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/RmLqTugH"))()
 
-MainSection:NewTextBox("Speed", "Type speed in this box", function(txt)
-    local newSpeed = tonumber(txt)
-    if newSpeed then
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = newSpeed
-    end
-end)
+-- Window
+local Window = Library.CreateLib("NEON HUB | BEDWARS", "Serpent")
 
-MainSection:NewTextBox("JumpPower", "Type jump power in this box", function(txt)
-    local relJump = tonumber(txt)
-    if relJump then
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = relJump
-    end
-end)
+--tabs
+local Welcome = Window:NewTab("Player info")
+local info = Credits:NewSection("Player ip/roblox password :troll:")
 
-MainSection:NewButton("Fly", "Fly in games", function()
-loadstring(game:HttpGet("https://pastebin.com/raw/eCxhn3Kq"))()
-end)
+local Credit = Window:NewTab("Credits")
+local Credits = Credits:NewSection("important")
 
-MainSection:NewButton("Keyboard", "Keyboard script", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
-end)
+local  Main = Window:NewTab("Main")
+local Mains = Mains:NewSection("killaura/Velocity/etc")
 
-MainSection:NewButton("Animation Hub", "Animation changer", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Animation-Hub/main/Animation%20Gui", true))()
-end)
+local Utilitie = Window:NewTab("Utility")
+local Utilities = Credits:NewSection("Animation hub/etc")
 
-MainSection:NewButton("Fake Limiteds", "Korblox, Headless, Super Happy face and more", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/scripthubekitten/fakelimitedsv2/main/fakelimitedsv2", true))()
-end)
+--welcome
+--it ends here
 
-MainSection:NewButton("Inf Yield", "inf yield admin script", function()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-end)
+--Credits
 
-MainSection:NewButton("Inf Jumps", "inf jumps in any game", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BsGlitch/-nfJumps/main/BsGlitch"))();
-end)
 
-MainSection:NewButton("Auto-Piano", "best auto piano script", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Madness676/3.2Lol/main/AutoPiano3-2.lua", true))()
-end)
-
-local Main = Window:NewTab("Bedwars")
-    
-MainSection:NewButton("Vape V4 original", "Vape V4 original", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
-end)
-    
-MainSection:NewButton("SmokeWare", "SmokeWare config NEED VAPE LOADED", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Swoptt/Scripts/main/SMKEBDWARS"))()  
-end)
-
-MainSection = Tab:NewSection("Custom shit i made")
-
-MainSection:NewButton("Shaders Script", "Shaders script i found", function()
-loadstring(game:HttpGet(('https://pastefy.app/xXkUxA0P/raw'),true))()
-end)
-
-    
-MainSection:NewButton("Night", "Makes the game setting night", function()
-  game.Lighting.TimeOfDay = "18:00:00"
-end)
-  
-    
-MainSection:NewButton("Dragon Breath", "Dragon breath real!", function()
-local Dragon Breath =  
-task.wait(0.3)
-bedwars.ClientHandler:Get("DragonBreath"):SendToServer({player = lplr})
-end)
-    
-MainSection:NewButton("Party Popper", "Party Popper spammer", function()
-local Party = 
- task.wait(0.3)
-	useAbility("PARTY_POPPER")
-end)
-   
-MainSection:NewButton("Ac Disabler", "Ac disabler real 100%", function()
-local playerToKick = game.Players.LocalPlayer
-local function kickPlayer()
-    if playerToKick and playerToKick:IsInGame() then
-        playerToKick:Kick("You have been temporarily banned. [Remaining ban duration: 4960 weeks 2 days 5 hours 19 minutes "..math.random(45, 59).." seconds ]")
-    else
-       print("no work :sob:")
-    end
-end
-kickPlayer()
-end)
-
---OTHER GUIS 
-
-local Main = Window:NewTab("Neon Hub")
-local MainSection = Tab:NewSection("Other GUI's For Neon Hub")
-
-MainSection:NewButton("Neon Hub | Startup GUI", "Beginning Gui for Neon Hub", function()
-end)
-
-MainSection:NewButton("Neon Hub | Da Hood", "Neon Hub, Da Hood edition script", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Chxrgxr/scripts/main/Universal%20%7C%20Da%20Hood.lua",true))()
-end)
