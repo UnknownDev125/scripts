8264e51fdfcd4f6c0b761af17308131b2720a694
Unknown | Dev • <107710524+UnknownDev125@users.noreply.github.com>
2023-12-30 23:48:38 +0800

    Updated NeonHub | Bedwars.lua

--- NeonHub | Bedwars.lua
+++ NeonHub | Bedwars.lua
@@ -5,11 +5,11 @@ local Library = loadstring(game:HttpGet("https://pastebin.com/raw/RmLqTugH"))()
 local Window = Library.CreateLib("NEON HUB | BEDWARS", "Serpent")
 
 --tabs
-local Main = Window:NewTab("Main")
-local Mains = Main:NewSection("KillAura/Combat Stuffs")
-
 local Information = Window:NewTab("Player info")
-local info = Information:NewSection("Player name/roblox info")
+local Info = Information:NewSection("Player name/roblox info")
+
+local Combat = Window:NewTab("Combats")
+local Combats = Combat:NewSection("KillAura/Combat Stuffs")
 
 local Credit = Window:NewTab("Credits")
 local Credits = Credit:NewSection("important")
@@ -19,30 +19,18 @@ local Utilities = Utility:NewSection("Animation hub/etc")
 
 --Variables
 local players = game:GetService("Players")
-
-if players then
-    game:GetService("StarterGui"):SetCore("SendNotification",{
-        Title = "Notification",
-        Text = "Player Found! ",name,
-    })
     local localplayer = players.LocalPlayer
     local name = localplayer.Name
     local userid = localplayer.UserId
     local accountage = localplayer.AccountAge
-end
-    else
-        print("Player Not Found!")
-        game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = "Notification",
-            Text = "Player Not Found!",
-        })
 
 --Information
-Information:NewButton("Player Name: ",name, "Shows The Players Name", function()
+
+Info:NewButton("Player Name: "..name, "Shows The Players Name", function()
 end)    
 
-Information:NewButton("Player Id:",userid, "Shows The Players Id", function()
+Info:NewButton("Player Id: "..userid, "Shows The Players Id", function()
 end)
 
-Information:NewButton("Player Age",accountage, "Shows The Players Account Age", function()
+Info:NewButton("Player Age: "..accountage, "Shows The Players Account Age", function()
 end)
